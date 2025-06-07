944007f87919c3b8bceabd93a634ff2e4121a4ac
ssss442 <sararegi759@gmail.com>
2025-06-07 11:29:04 -0300

    Add files via upload

---
+++ operações basicas div zero.py
@@ -0,0 +1,19 @@
+def main():
+  num1 = int(input("Digite um numero: "))
+  num2 = int(input("Digite outro numero: ")) 
+  
+  soma = num1 + num2
+  sub = num1 - num2
+  mult = num1 * num2
+  if num2 == 0:
+   div = "Impossivel dividir por zero" 
+  else:
+    div = num1 / num2 
+  
+  print("A soma de ", num1," + ", num2," = ", soma)
+  print("A subtração de ", num1," - ", num2," = ", sub)
+  print("A multiplicação de ", num1," * ", num2," = ", mult)
+  print("A divisão de ", num1," / ", num2," = ", div)
+
+  return 0
+main()
