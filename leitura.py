089bcd6e4d7909f56234f91d860050901c6a7efe
ssss442 <sararegi759@gmail.com>
2025-06-07 10:45:09 -0300

    Add files via upload

---
+++ leitura.py
@@ -0,0 +1,11 @@
+def main():
+  nome = input("Digite seu nome: ")
+  idade = int(input( "Digite sua idade: "))
+  altura = float(input("Digite sua altura: ")) 
+  isFeminino = input("Voce é do sexo feminino? (True/False):")
+  print("Ola" , nome)
+  print("idade" , idade)
+  print("altura" , altura)
+  print("éFeminino" , isFeminino)
+  return 0
+main()
